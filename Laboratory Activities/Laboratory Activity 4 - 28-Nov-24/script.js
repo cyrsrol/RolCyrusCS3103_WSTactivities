fddb2a6dc191dd1d4491285bcb0d42c757e14e71
Cyrus Rol <130523706+cyrsrol@users.noreply.github.com>
2024-11-30 14:12:21 +0800

    Update script.js

--- Laboratory Activities/Laboratory Activity 4 - 28-Nov-24/script.js
+++ Laboratory Activities/Laboratory Activity 4 - 28-Nov-24/script.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cloudsContainer = document.querySelector('.clouds');
+
+    for (let i = 0; i < 20; i++) {
+        const cloud = document.createElement('div');
+        cloud.className = 'cloud';
+        cloudsContainer.appendChild(cloud);
+    }
+});
 
